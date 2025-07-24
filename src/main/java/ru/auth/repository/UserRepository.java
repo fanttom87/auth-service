50d4b5ec7a24b8306c75a7c37bcb78a567147675
RageMultiplayerRU <kaur04@list.ru>
2025-07-24 12:13:45 +0300

    add UserRepository

---
+++ src/main/java/ru/auth/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package ru.auth.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.auth.model.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLogin(String login);
+    
+    Optional<User> findByEmail(String email);
+}
