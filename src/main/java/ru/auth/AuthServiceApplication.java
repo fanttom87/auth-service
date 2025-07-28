28a350bb6a53734e9ba0c571a4a2b8a2b7b7ecab
RageMultiplayerRU <kaur04@list.ru>
2025-07-28 10:33:23 +0300

    Update AuthServiceApplication.java

--- src/main/java/ru/auth/AuthServiceApplication.java
+++ src/main/java/ru/auth/AuthServiceApplication.java
@@ -1,7 +1,11 @@
 package ru.auth;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AuthServiceApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
